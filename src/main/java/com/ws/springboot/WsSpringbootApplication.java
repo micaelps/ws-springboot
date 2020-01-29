ba9f209d1823fecce619ad0de5af779b700cb087
Micael Pinheiro <3.1416micael@gmail.com>
2020-01-29 17:49:31 -0300

    Project created

---
+++ src/main/java/com/ws/springboot/WsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ws.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSpringbootApplication.class, args);
+	}
+
+}
