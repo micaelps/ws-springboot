5d40b09cec67c96487485f7c664fa011b26e6961
Micael Pinheiro <3.1416micael@gmail.com>
2020-01-30 22:00:35 -0300

    Order, Instant, ISO 8601 PART 1

--- src/main/java/com/ws/springboot/entities/User.java
+++ src/main/java/com/ws/springboot/entities/User.java
@@ -1,14 +1,19 @@
 package com.ws.springboot.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -22,6 +27,8 @@ public class User implements Serializable {
 	private String password;
 	
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -85,7 +92,12 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -112,6 +124,9 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
+
+	
 	
 	
 	
